73ce5fe49d531fcd4e1c06b03abd5af4860e1b18
Brian <riceb53@gmail.com>
2018-08-05 11:09:39 -0500

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+
+
 Rails.application.routes.draw do
   namespace :api do
     get "/tasks" => "tasks#index"
@@ -5,5 +7,5 @@ Rails.application.routes.draw do
     get "/tasks/:id" => "tasks#show"
     patch "/tasks/:id" => "tasks#update"
     delete "/tasks/:id" => "tasks#destroy"
-  end  
+  end
 end
